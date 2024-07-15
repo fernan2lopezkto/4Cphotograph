e9e312c48cd43f0d3e83c7b8f10c2b6b41864342
JuanW11 <fernan2lopezkto@gmail.com>
2024-07-15 11:02:49 -0300

    f C

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import RespNavBar from './Components/NavBar';
+import App from './App.jsx'
+import './index.css'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RespNavBar />
+    <App />
+  </React.StrictMode>,
+)
